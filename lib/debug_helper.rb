e2430840ecb55ffc8ebe053974e5acc63f7e261a
burdettelamar <burdettelamar@yahoo.com>
2018-06-11 13:57:12 -0500

    More on puts_each_pair

--- lib/debug_helper.rb
+++ lib/debug_helper.rb
@@ -1,5 +1,31 @@
-require "debug_helper/version"
+require 'debug_helper/version'
 
 class DebugHelper
 
+  def self.respond_to!(obj, method)
+    unless obj.respond_to?(method)
+      message = "Instance of #{obj.class.name} does not respond to :#{method}"
+      raise ArgumentError.new(message)
+    end
+  end
+
+  def self.puts_each_pair(obj, label = obj.class.name)
+    self.respond_to!(obj, :each_pair)
+    lines = [
+        "Label:  #{label}",
+        "Count:  #{obj.size}",
+    ]
+    # Don't assume that we have :each_with_index.
+    i = 0
+    obj.each_pair do |key, value|
+      lines.push("  Pair #{i}:")
+      lines.push("    Key (#{key.class.name}): #{key.inspect}")
+      lines.push("    Value (#{value.class.name}): #{value.inspect}")
+      i += 1
+    end
+    lines.push('')
+    lines.join("\n")
+    puts lines
+    lines
+  end
 end
--- test/debug_helper_test.rb
+++ test/debug_helper_test.rb
@@ -1,9 +1,79 @@
+require 'diff-lcs'
+
 require "test_helper"
+
+TEST_DIR_PATH = File.dirname(__FILE__)
 
 class DebugHelperTest < Minitest::Test
 
   def test_version
     refute_nil ::DebugHelper::VERSION
+  end
+
+  def self.write_stdout(file_path)
+    old_stdout = $stdout
+    $stdout = StringIO.new
+    yield
+    File.write(file_path, $stdout.string)
+  ensure
+    $stdout = old_stdout
+  end
+
+  def test_puts_each_pair
+
+    method = :puts_each_pair
+    {
+        :empty => {},
+        :string_values => {:a => 'a', :b => 'b'},
+        :symbol_values => {:a => :a, :b => :b},
+        :nil_values => {:a => nil, :b => nil},
+        :int_keys => {0 => 'a', 1 => 'b'},
+    }.each_pair do |name, hash|
+      actual_file_path = File.join(
+          TEST_DIR_PATH,
+          method.to_s,
+          'actual',
+          "#{name.to_s}.txt",
+      )
+      DebugHelperTest.write_stdout(actual_file_path) do
+        DebugHelper.send(method, hash, name.to_s)
+      end
+      expected_file_path = File.join(
+          TEST_DIR_PATH,
+          method.to_s,
+          'expected',
+          "#{name.to_s}.txt",
+      )
+      diffs = DebugHelperTest.diff_files(expected_file_path, actual_file_path)
+      assert_empty(diffs)
+
+    end
+
+    # Test invalid objects.
+    [
+        'foo',
+        nil,
+        true,
+        Object.new,
+    ].each do |obj|
+      e = assert_raises(ArgumentError) do
+        DebugHelper.puts_each_pair(obj)
+      end
+      assert_equal("Instance of #{obj.class.name} does not respond to :each_pair", e.message)
+    end
+
+  end
+
+  def self.diff_files(expected_file_path, actual_file_path)
+    diffs = nil
+    File.open(expected_file_path) do |expected_file|
+      expected_lines = expected_file.readlines
+      File.open(actual_file_path) do |actual_file|
+        actual_lines = actual_file.readlines
+        diffs = Diff::LCS.diff(expected_lines, actual_lines)
+      end
+    end
+    diffs
   end
 
 end
