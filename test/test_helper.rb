27c94d7b58d26aee15d5862e164c3d32ade38ab9
burdettelamar <burdettelamar@yahoo.com>
2018-07-04 14:28:33 -0500

    More on putd

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "debug_helper"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'debug_helper'
 
 require "minitest/autorun"
