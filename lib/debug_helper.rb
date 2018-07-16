1d9c205e825268415b0cd15ee09bfde80d17a0a5
burdettelamar <burdettelamar@yahoo.com>
2018-07-15 19:14:57 -0500

    More on refactor

--- lib/debug_helper.rb
+++ lib/debug_helper.rb
@@ -101,10 +101,11 @@ class DebugHelper
     def show
       pair_name, key_name, value_name = *pair_names
       content = {}
-      obj.each_with_index do |pair, i|
-        key, value = *pair
+      i = 0
+      obj.each_pair do |key, value|
         pair = {key_name => debug_helper._show(key, nil, {}), value_name => debug_helper._show(value, nil, {})}
-        content.store("Pair #{i}", pair)
+        content.store("#{pair_name} #{i}", pair)
+        i += 1
       end
       debug_helper._show_item(obj.class.name, content, attrs, info)
     end
